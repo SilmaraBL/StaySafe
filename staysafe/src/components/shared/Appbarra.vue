71bc3642dc3a2ca525061b334f823c322a284c2d
yasmin menashe <menasheyy@gmail.com>
2021-07-18 03:44:02 -0300

    listaApi

--- staysafe/src/components/shared/Appbarra.vue
+++ staysafe/src/components/shared/Appbarra.vue
@@ -78,3 +78,7 @@ export default {
     }),
 } 
 </script>
+
+<style scoped>
+
+</style>
--- staysafe/src/views/Prof.vue
+++ staysafe/src/views/Prof.vue
@@ -1,6 +1,33 @@
 <template>
     <v-container> 
   <h1 class="text-h8 text-center mb-3 mt-5">Profissionais</h1>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+          Nome
+          </th>
+          <th class="text-left">
+           Especialidade
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="profissao of prof" :key="profissao.id">
+          <td>
+            <v-avatar>
+      <img
+        :src="profissao.escudo"
+        :alt="profissao.nome"
+      >
+    </v-avatar>
+            {{profissao.nome}}</td>
+           <td>{{profissao.servicos}}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
     </v-container>
 </template>
 
@@ -16,8 +43,10 @@ export default {
     fetch('https://it3-gdf-default-rtdb.firebaseio.com/saude/profissionais.json')
       .then(response => response.json())
       .then(json => {
-        this.clubesLista = json;
+        this.Prof = json;
+         console.log(this.Prof);
       })
+     ;
   }
 }
 </script>

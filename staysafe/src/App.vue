60098c228e444ed3fa03bdbd029a9c295d31b149
yasmin menashe <menasheyy@gmail.com>
2021-07-17 23:38:47 -0300

    addrouter

--- staysafe/src/App.vue
+++ staysafe/src/App.vue
@@ -1,60 +1,7 @@
 <template>
   <v-app>
-<v-app-bar app color="purple darken-1" dark >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-     <v-toolbar-title>Fique Em Casa</v-toolbar-title>
-    </v-app-bar>
+   <Appbarra/>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="purple darken-1"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-chart-bubble</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Quem Somos</v-list-item-title>
-          </v-list-item>
-        
-           <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account-group</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Profissionais</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Cadastre-se</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account-box</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Contato</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
       <Stayhome/>
     </v-main>
@@ -63,14 +10,12 @@
   
 <script>
 import Stayhome from  './components/Stayhome.vue'
+import Appbarra from './components/shared/Appbarra.vue'
   export default {
     name: 'app',
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
     components: {
-    Stayhome
+    Stayhome,
+    Appbarra
     }
   };
 </script>
---
+++ staysafe/src/components/shared/Appbarra.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="app-barra">
+        <v-app-bar app color="purple darken-1" dark >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+     <v-toolbar-title>Fique Em Casa</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="purple darken-1"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-chart-bubble</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Quem Somos</v-list-item-title>
+          </v-list-item>
+        
+           <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account-group</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Profissionais</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Cadastre-se</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account-box</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Contato</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'Appbarra',
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+} 
+</script>
